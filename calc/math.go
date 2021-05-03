1994cc0e25f8b7e51a9798324da439b8a1cea71a
yukasa <yukasakai.net@gmail.com>
2021-05-03 12:50:53 +0800

    first commit

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,6 @@
 package calc
 
 // returns sum of two integers
-func Add(numbers ...int) int {
-	sum := 0
-
-	for _, num := range numbers {
-		sum = sum + num
-	}
-
-	return sum
+func Add(i int, j int) int {
+	return i + j
 }

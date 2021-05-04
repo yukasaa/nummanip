e72d803ed65f91c87989fa0636de98c82a5b5016
yukasa <yukasakai.net@gmail.com>
2021-05-04 12:19:12 +0800

    new release version 2.0.0

--- calc/math.go
+++ calc/math.go
@@ -1,12 +1,18 @@
 package calc
 
-// returns sum of two integers
-func Add(numbers ...int) int {
+import "errors"
+
+// returns sum of two integers with error
+func Add(numbers ...int) (error, int) {
 	sum := 0
 
-	for _, num := range numbers {
-		sum = sum + num
-	}
+	if len(numbers) < 2 {
+		return errors.New("provide more than 2 numbers"), sum
+	} else {
+		for _, num := range numbers {
+			sum = sum + num
+		}
 
-	return sum
+		return nil, sum
+	}
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,3 @@
-module github.com/yukasaa/nummanip
+module github.com/yukasaa/nummanip/v2
 
 go 1.16
